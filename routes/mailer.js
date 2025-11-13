f27be70ff0b032c70eff628d169b0babb977d78f
sj-10ps <sps24723@gmail.com>
2025-11-13 15:57:54 +0530

    fixea

--- routes/mailer.js
+++ routes/mailer.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
+  port:465,
+  secure:true,
   auth: {
     user: process.env.NODEMAILUSER,
     pass: process.env.NODEMAILPASS,
