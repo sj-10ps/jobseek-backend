7e699fc8b24c2c6b89e7ea90249fee8698c4cad0
sj-10ps <sps24723@gmail.com>
2025-11-14 19:14:55 +0530

    fixes

--- routes/Company.js
+++ routes/Company.js
@@ -9,12 +9,19 @@ const multer = require('multer')
 const {transporter}=require('./mailer')
 const  {Resend}=require('resend')
 const resend=new Resend(process.env.RESENDURL)
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // folder where files will be stored
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); // unique file name
+const { v2: cloudinary } = require('cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+cloudinary.config({
+   cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params:{
+    folder:'posts'
   }
 });
 
